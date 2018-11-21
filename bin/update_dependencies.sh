8c11826a5095c81fe2d6be643393d30ebcc57f64
Robert Hafner <tedivm@tedivm.com>
2018-11-20 16:16:06 -0800

    Exclude beta releases from upgrade script

--- bin/update_dependencies.sh
+++ bin/update_dependencies.sh
@@ -18,7 +18,7 @@ while read p; do
   program=$(echo $p | cut -d= -f1)
   curr_version=$(cat ./requirements.txt | grep "${program}==" | cut -d'=' -f3 | sed 's/[^0-9.]*//g')
   PACKAGE_JSON_URL="https://pypi.python.org/pypi/${program}/json"
-  version=$(curl -L -s "$PACKAGE_JSON_URL" | jq  -r '.releases | keys | .[]' | sort -V | tail -1 | sed 's/[^0-9.]*//g' )
+  version=$(curl -L -s "$PACKAGE_JSON_URL" | jq  -r '.releases | keys | .[]' | sed '/^[^[:alpha:]]*$/!d' | sort -V | tail -1 | sed 's/[^0-9.]*//g' )
 
   if [ "$curr_version" != "$version" ]; then
     echo "Updating requirements for ${program} from ${curr_version} to ${version}."
